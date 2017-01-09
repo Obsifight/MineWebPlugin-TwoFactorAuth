326e9fee40b16040b6035f40e47bacf03531c1eb
Valentin <contact@eywek.fr>
2017-01-09 20:27:13 +0100

    fix route for new controller name

--- Config/routes.php
+++ Config/routes.php
@@ -1,5 +1,5 @@
 <?php
-Router::connect('/user/two-factor-auth/valid', array('plugin' => 'TwoFactorAuth', 'admin' => false, 'controller' => 'user', 'action' => 'validLogin'));
+Router::connect('/user/two-factor-auth/valid', array('plugin' => 'TwoFactorAuth', 'admin' => false, 'controller' => 'UserLogin', 'action' => 'validLogin'));
 Router::connect('/user/two-factor-auth/generate', array('controller' => 'UserLogin', 'action' => 'generateSecret', 'plugin' => 'TwoFactorAuth'));
 Router::connect('/user/two-factor-auth/valid-enable', array('controller' => 'UserLogin', 'action' => 'validEnable', 'plugin' => 'TwoFactorAuth'));
 Router::connect('/user/two-factor-auth/disable', array('controller' => 'UserLogin', 'action' => 'disable', 'plugin' => 'TwoFactorAuth'));
